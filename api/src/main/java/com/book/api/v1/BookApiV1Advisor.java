819a66723f4ff22bf6089a144770d7f32254c1f7
Captain Mcwiise <mcwiise@gmail.com>
2025-10-15 14:00:32 -0500

    remove unused imports

--- api/src/main/java/com/book/api/v1/BookApiV1Advisor.java
+++ api/src/main/java/com/book/api/v1/BookApiV1Advisor.java
@@ -1,29 +1,14 @@
 package com.book.api.v1;
 
-import com.book.api.v1.dto.BookDto;
-import com.book.api.v1.dto.CreateBookDto;
 import com.book.api.v1.dto.HttpErrorBody;
-import com.book.domain.Book;
 import com.book.domain.exception.BookNotFoundException;
-import com.book.usecase.SimpleUseCase;
-import com.book.usecase.UseCase;
-import jakarta.validation.Valid;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
 
 @ControllerAdvice
 @RestController
